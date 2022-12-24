6979f07a314053fe893a3662a150c36e45c2ba11
zyt <2947060232@qq.com>
2022-12-24 13:42:15 +0800

    1.imread imshow

---
+++ bulid/CMakeFiles/ImageShow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageShow.dir/main.cpp.o"
+  "CMakeFiles/ImageShow.dir/main.cpp.o.d"
+  "ImageShow"
+  "ImageShow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageShow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
