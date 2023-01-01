32a799dd353ded8c666f38402127d70293eb3a7a
zyt <2947060232@qq.com>
2023-01-01 23:49:17 +0800

    OpenCV任务10

---
+++ countCoins/bulid/CMakeFiles/countCoins.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/countCoins.dir/main.cpp.o"
+  "CMakeFiles/countCoins.dir/main.cpp.o.d"
+  "countCoins"
+  "countCoins.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/countCoins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ countCoins/bulid/CMakeFiles/countCoins.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for countCoins.
+# This may be replaced when dependencies are built.
